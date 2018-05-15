dc71ec6772aa70489e8ced5610dbd10f160c137b
Henri-Gerard <hd.gerard@orange.fr>
2018-05-15 16:16:20 +0200

    [FIX] Fix download error in julia 06 switching from HTTPClient to HTTP

--- src/UCIMLRepo.jl
+++ src/UCIMLRepo.jl
@@ -1,45 +1,45 @@
 module UCIMLRepo
 
-using HTTPClient.HTTPC
+using HTTP
 using DataArrays
 using DataFrames
 using PyCall
 
 @pyimport lxml.html as lh
 
-export ucirepodata,ucirepoinfo,ucirepolist 
+export ucirepodata,ucirepoinfo,ucirepolist
 
 
-function download_http(url::ASCIIString)
+function download_http(url::String)
     println("fetching from the following url : ",url)
-    request = HTTPC.get(url)
-    if request.http_code != 200
+    request = HTTP.request("GET", url)
+    if request.status != 200
         error("download failed")
-    end	 
-#converting received data into ASCII format and returning the value     
-    rcvd_data = bytestring(request.body) 
+    end
+#converting received data into ASCII format and returning the value
+    rcvd_data = String(request.body)
 end
 
 #This function converts the input string into a dataframe
-function parse_rcvd_data(rcvd_data::ASCIIString)
-    rows = ASCIIString[]
-    rows = split(rcvd_data,'\n',false)
+function parse_rcvd_data(rcvd_data::String)
+    rows = String[]
+    rows = split(rcvd_data,'\n',keep = false)
     df = DataFrame()
-    
+
     for i = 1:size(rows)[1]
-        df[i] = split(rows[i],',',true)	
+        df[i] = split(rows[i],',',keep =true)
     end
-    df		
+    df
 end
 
 #TODO: Use readtable("file.csv") instead of splitting the array.
 
-function parse_rcvd_info(rcvd_info::ASCIIString)
-    rows = ASCIIString[]
-    rows = split(rcvd_info,'\n',false)
-	
+function parse_rcvd_info(rcvd_info::String)
+    rows = String[]
+    rows = split(rcvd_info,'\n',keep =false)
+
     for i = 1:size(rows)[1]
-        println(rows[i])	
+        println(rows[i])
     end
 end
 #TODO: used the parsed info to create the header line for the dataframe
@@ -49,18 +49,18 @@ end
 # you can alternatively specify the exact url to *any* .data file that you want to load as so:
 # df = ucirepodata("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data",false)
 
-function ucirepodata(DsetNameOrUrl::ASCIIString, DorU::Bool=true)
+function ucirepodata(DsetNameOrUrl::String, DorU::Bool=true)
     if(DorU)
-#creating the url to be used for download 
+#creating the url to be used for download
         url = string("http://archive.ics.uci.edu/ml/machine-learning-databases/", DsetNameOrUrl,"/",DsetNameOrUrl,".data")
-	
+
     else
         url = DsetNameOrUrl
     end
-	
-#loading received data into a string type variable 
+
+#loading received data into a string type variable
     rcvd_data = download_http(url)
-	
+
     df = DataFrame()
 #parsing received data and loading into an appropriate dataframe
     df = parse_rcvd_data(rcvd_data)
@@ -72,17 +72,17 @@ end
 # you can alternatively specify the exact url to *any* .data file that you want to load and view, as so:
 # ucirepoinfo("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.names",false)
 
-function ucirepoinfo(DsetNameOrUrl::ASCIIString, DorU::Bool=true)
+function ucirepoinfo(DsetNameOrUrl::String, DorU::Bool=true)
     if(DorU)
-	#creating the url to be used for download	
+	#creating the url to be used for download
         url = string("http://archive.ics.uci.edu/ml/machine-learning-databases/", DsetNameOrUrl,"/",DsetNameOrUrl,".names")
-	
+
     else
         url = DsetNameOrUrl
     end
-	
-#loading received data into a string type variable 
-    rcvd_info = download_http(url) 
+
+#loading received data into a string type variable
+    rcvd_info = download_http(url)
 
 #parsing recieved info and displaying on STDOUT
     parse_rcvd_info(rcvd_info)
@@ -98,11 +98,11 @@ function ucirepolist()
     text = doc[:xpath]("/html//table[2]//table[2]")
     rows = text[2][:xpath]("/html//table[2]//table[2]//tr[1]")
     i =2;
-    	
-    	
-    print("name|default task\n") 				
-    while i<= 284                                                                       
-	 
+
+
+    print("name|default task\n")
+    while i<= 284
+
          name 			= rows[1][:xpath](string("""/html//table[2]//td[2]/table[2]//tr[""",i,"""]//td[2]/p//text()"""))[1]
          dtype 			= rows[1][:xpath](string("""/html//table[2]//td[2]/table[2]//tr[""",i,"""]//td[2]/p//text()"""))[2]
          #dtask[i-1] 		= rows[1][:xpath](string("""/html//table[2]//td[2]/table[2]//tr[""",i,"""]//td[3]/p//text()"""))
@@ -110,8 +110,8 @@ function ucirepolist()
          #instances[i-1] 	= rows[1][:xpath](string("""/html//table[2]//td[2]/table[2]//tr[""",i,"""]//td[5]/p//text()"""))
          #attributes[i-1] 	= rows[1][:xpath](string("""/html//table[2]//td[2]/table[2]//tr[""",i,"""]//td[6]/p//text()"""))
          #year[i-1] 		= rows[1][:xpath](string("""/html//table[2]//td[2]/table[2]//tr[""",i,"""]//td[7]/p//text()"""))
-	
-	 print(name," | ", dtype,"\n")	
+
+	 print(name," | ", dtype,"\n")
 	 i += 1
     end
 
